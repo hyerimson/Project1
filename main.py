5e2c9af367cf41102b2060c79c21d181951c1dd1
SonHyeRim <hlson1004@gmail.com>
2023-07-20 16:45:02 +0900

    제곱함수 추가 제곱함수 추가한 버전 스태시 스태시 적용 후 커밋으로까지 보냅니다

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def add_func(n1,n2):
 def sub_func(n1,n2):
     return n1-n2
 
+def mul_func(n1,n2):
+    return n1*n2
+
 ###전역 변수부
 num1, num2, res = 100,200,0
 
@@ -23,3 +26,6 @@ print(num1,'+',num2,'=',res)
 res=sub_func(num1, num2)
 print(num1,'-',num2,'=',res)
 
+###곱하기 함수 추가
+res=mul_func(num1, num2)
+print(num1,'*',num2,'=',res)
