ca3e76e3430f53b683b2cff9f36dc1c0708c019f
SonHyeRim <hlson1004@gmail.com>
2023-07-20 14:33:07 +0900

    두번째 커밋 빼기 함수 추가~

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ Created on Thu Jul 20 14:22:56 2023
 def add_func(n1,n2):
     return n1+n2
 
+def sub_func(n1,n2):
+    return n1-n2
+
 ###전역 변수부
 num1, num2, res = 100,200,0
 
 ###메인 코드부
 res=add_func(num1, num2)
 print(num1,'+',num2,'=',res)
+
+###빼기 함수 추가
+res=sub_func(num1, num2)
+print(num1,'-',num2,'=',res)
+
